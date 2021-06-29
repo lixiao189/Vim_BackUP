3db3b8fb37617b489c95819f7b89c14ba81c2529
Node Sans <lixiao189@outlook.com>
2021-06-30 00:31:05 +0800

    Add some dependencies in instal.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,13 @@ brew install vim
 brew install node
 brew install llvm
 brew install yarn
+
+# The dependencies of the fzf plugin
 brew install fzf
 brew install bat
+brew install the_silver_searcher
+brew install ripgrep
+
 echo "export BAT_THEME='GitHub'" >> ~/.zshrc
+# Set the registry of the npm
 npm config set registry https://registry.npm.taobao.org
